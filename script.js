5c247388376bfc8ded4ccb695f8d66e99e6679f0
Luis Rosario <luisalmonter@gmail.com>
2021-03-22 18:22:39 -0400

    adds hour hand function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
@@ -14,9 +15,12 @@ function setDate() {
 
     minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
 
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
-
+    console.log(hours);
 
 
 }
