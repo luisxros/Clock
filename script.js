65480b325259eea7aeb1e0cffb911f3f4117b80d
Luis Rosario <luisalmonter@gmail.com>
2021-03-22 18:15:19 -0400

    adds minute hand feature

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const secondsDegree = ((seconds / 60) * 360);
-    console.log(now);
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minsDegrees = ((minutes / 60) * 360) + 90;
+
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+
+
+
+
+
 }
 
 setInterval(setDate, 1000);
